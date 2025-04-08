0ccd928f6100aadd9df4d9beeedf5d20bf538879
Eduardo Fernando Farfus <eduardo.farfus@angellira.com.br>
2025-04-08 17:58:40 -0300

    fea: finished structure chatlist

--- app/src/main/java/com/angellira/whatsapp2/features/chatsList/ChatsListScreen.kt
+++ app/src/main/java/com/angellira/whatsapp2/features/chatsList/ChatsListScreen.kt
@@ -9,8 +9,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
@@ -20,6 +24,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -31,60 +36,59 @@ import com.angellira.whatsapp2.ui.theme.WhatsApp2Theme
 fun ChatsListScreen(modifier: Modifier = Modifier) {
     LazyColumn(modifier.fillMaxSize(),
         contentPadding = PaddingValues(16.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        verticalArrangement = Arrangement.spacedBy(20.dp)) {
         //search textfields
         item {
             Row(
-                Modifier
-                    .clip(CircleShape)
-                    .padding(16.dp)
-                    .background(Color.Gray)
+                modifier = Modifier
                     .fillMaxWidth()
+                    .height(48.dp)
+                    .clip(CircleShape)
+                    .background(Color.Gray)
+                    .padding(horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 Icon(Icons.Default.Search, contentDescription = null)
-                Spacer(
-                    Modifier.size(8.dp)
-                )
+                Spacer(modifier = Modifier.width(8.dp))
                 Text(text = "Search...")
             }
         }
+
         item {
             Row(
                 horizontalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.fillMaxWidth()
             ) {
-                val filters = remember {
-                    mutableListOf("All", "Unread", "Groups")
-                }
-
-
+                val filters = remember { listOf("All", "Unread", "Groups") }
                 filters.forEach { filter ->
                     Box(
                         modifier = Modifier
-                            .padding(16.dp, 8.dp)
                             .clip(CircleShape)
                             .background(Color.Gray)
+                            .padding(horizontal = 16.dp, vertical = 8.dp)
                     ) {
                         Text(text = filter)
                     }
                 }
             }
         }
+
         items(12) {
+            val avatarSize = 60.dp
             Row(Modifier.fillMaxWidth()) {
                 Box(
                     Modifier
                         .clip(CircleShape)
                         .background(Color.Gray)
-                        .size(54.dp)
+                        .size(avatarSize)
                 ) {
 
                 }
-                Spacer(Modifier.size(8.dp))
+                Spacer(Modifier.size(16.dp))
                 Column(
                     Modifier
-                        .fillMaxWidth()
-                        .weight(1f)
-                        .padding(8.dp)
+                        .heightIn(avatarSize),
+                    verticalArrangement = Arrangement.Center,
                 ) {
                     Row(
                         Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween
@@ -98,6 +102,7 @@ fun ChatsListScreen(modifier: Modifier = Modifier) {
                     ) {
                         Row(Modifier.weight(1f)) {
                             Icon(Icons.Default.DoneAll, contentDescription = null)
+                            Spacer(Modifier.size(4.dp))
                             Text(text = "last massage")
                         }
 
