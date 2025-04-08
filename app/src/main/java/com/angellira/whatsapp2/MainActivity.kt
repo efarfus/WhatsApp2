7752ad398bd05e136586527b29091c108cbc4dd8
Eduardo Fernando Farfus <eduardo.farfus@angellira.com.br>
2025-04-08 16:33:27 -0300

    refactor: classes to switch topAppBar icons

--- app/src/main/java/com/angellira/whatsapp2/MainActivity.kt
+++ app/src/main/java/com/angellira/whatsapp2/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.angellira.whatsapp2
 
-import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -20,7 +19,7 @@ import androidx.compose.material.icons.filled.CameraAlt
 import androidx.compose.material.icons.filled.DataSaverOff
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.People
-import androidx.compose.material3.BottomAppBar
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -55,62 +54,92 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-sealed class NavItem(
+class BottomAppBarItem(
     val icon: ImageVector,
     val label: String,
+)
+
+class TopAppBarItem(
+    val title: String,
+    val icon: List<ImageVector> = emptyList(),
+)
+
+sealed class ScreenItem(
+    val topAppItem: TopAppBarItem, val bottomAppItem: BottomAppBarItem
 ) {
-    data object Chats : NavItem(
-        icon = Icons.AutoMirrored.Filled.Message,
-        label = "Chats"
+    data object Chats : ScreenItem(
+        bottomAppItem = BottomAppBarItem(
+            icon = Icons.AutoMirrored.Filled.Message, label = "Chats"
+        ), topAppItem = TopAppBarItem(
+            title = "WhatsApp 2", icon = listOf(
+                Icons.Default.CameraAlt, Icons.Default.MoreVert
+            )
+        )
     )
 
-    data object Status : NavItem(
-        icon = Icons.Default.DataSaverOff,
-        label = "Status"
+    data object Status : ScreenItem(
+        bottomAppItem = BottomAppBarItem(
+            icon = Icons.Default.DataSaverOff, label = "Status"
+        ), topAppItem = TopAppBarItem(
+            title = "Status", icon = listOf(
+                Icons.Default.Search, Icons.Default.MoreVert
+            )
+        )
+
     )
 
-    data object Communities : NavItem(
-        icon = Icons.Default.People,
-        label = "Communities"
+    data object Communities : ScreenItem(
+        bottomAppItem = BottomAppBarItem(
+            icon = Icons.Default.People, label = "Communities"
+        ), topAppItem = TopAppBarItem(
+            title = "Communities", icon = listOf(
+                Icons.Default.MoreVert
+            )
+        )
     )
 
-    data object Calls : NavItem(
-        icon = Icons.Default.Call,
-        label = "Calls"
+    data object Calls : ScreenItem(
+        bottomAppItem = BottomAppBarItem(
+            icon = Icons.Default.Call, label = "Calls"
+        ), topAppItem = TopAppBarItem(
+            title = "Calls", icon = listOf(
+                Icons.Default.Search, Icons.Default.MoreVert
+            )
+        )
     )
 }
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 private fun App() {
-    val items = remember {
+    val screens = remember {
         listOf(
-            NavItem.Chats,
-            NavItem.Status,
-            NavItem.Communities,
-            NavItem.Calls
+            ScreenItem.Chats, ScreenItem.Status, ScreenItem.Communities, ScreenItem.Calls
         )
 
     }
-    var selectedItem by remember { mutableStateOf(items.first()) }
-    val pagerState = rememberPagerState { items.size }
-    LaunchedEffect(selectedItem) {
+    var currentScreen by remember { mutableStateOf(screens.first()) }
+    val pagerState = rememberPagerState { screens.size }
+    LaunchedEffect(currentScreen) {
         pagerState.animateScrollToPage(
-            items.indexOf(selectedItem)
+            screens.indexOf(currentScreen)
         )
     }
     LaunchedEffect(pagerState.targetPage) {
-        selectedItem = items[pagerState.targetPage]
+        currentScreen = screens[pagerState.targetPage]
     }
     Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
         TopAppBar(
-            title = { Text("WhatsApp 2") },
+            title = { Text(currentScreen.topAppItem.title) },
             actions = {
                 Row(
                     Modifier.padding(8.dp), horizontalArrangement = Arrangement.spacedBy(16.dp)
                 ) {
-                    Icon(Icons.Default.CameraAlt, contentDescription = "Camera")
-                    Icon(Icons.Default.MoreVert, contentDescription = "More")
+                    currentScreen.topAppItem.icon.forEach { icon ->
+                        Icon(
+                            icon, contentDescription = null,
+                        )
+                    }
                 }
             },
 
@@ -118,35 +147,30 @@ private fun App() {
 
     }, bottomBar = {
         NavigationBar {
-            items.forEach { navItem ->
-                NavigationBarItem(
-                    selected = navItem == selectedItem,
-                    onClick = {
-                        selectedItem = navItem
+            screens.forEach { screen ->
+                with(screen.bottomAppItem) {
+                    NavigationBarItem(selected = screen == currentScreen, onClick = {
+                        currentScreen = screen
                     },
-
-                    icon = {
-                        Icon(
-                            imageVector = navItem.icon,
-                            contentDescription = navItem.label,
-                        )
-                    },
-                    label = { Text(navItem.label) }
-                )
+                        icon = {
+                            Icon(
+                                icon, label
+                            )
+                        }, label = { label })
+                }
             }
 
         }
-}) {
-    innerPadding ->
-    HorizontalPager(pagerState, Modifier.padding(innerPadding)) { page ->
-        when (items[page]) {
-            is NavItem.Chats -> ChatsListScreen()
-            is NavItem.Status -> UpdatesScreen()
-            is NavItem.Communities -> CommunitiesScreen()
-            is NavItem.Calls -> CallsScreen()
+    }) { innerPadding ->
+        HorizontalPager(pagerState, Modifier.padding(innerPadding)) { page ->
+            when (screens[page]) {
+                is ScreenItem.Chats -> ChatsListScreen()
+                is ScreenItem.Status -> UpdatesScreen()
+                is ScreenItem.Communities -> CommunitiesScreen()
+                is ScreenItem.Calls -> CallsScreen()
+            }
         }
     }
-}
 }
 
 
